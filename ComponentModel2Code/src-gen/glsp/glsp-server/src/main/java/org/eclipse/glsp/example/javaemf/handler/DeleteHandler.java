3b7d1751426c34e30b176eb005a7a9f4d60d7477
Botchi24 <botchi@web.de>
2024-11-16 18:50:19 +0100

    delete edge manually coded

--- ComponentModel2Code/src-gen/glsp/glsp-server/src/main/java/org/eclipse/glsp/example/javaemf/handler/DeleteHandler.java
+++ ComponentModel2Code/src-gen/glsp/glsp-server/src/main/java/org/eclipse/glsp/example/javaemf/handler/DeleteHandler.java
@@ -14,7 +14,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR MIT
  ********************************************************************************/
-package [%=_packageName%].handler;
+package org.eclipse.glsp.example.javaemf.handler;
 
 import java.util.ArrayList;
 import java.util.List;
